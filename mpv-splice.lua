8a55c460507fb08f3ee56788b2a6d8c6f928a03a
automatata <dt0bc7@gmail.com>
2020-10-13 11:57:36 +0800

    Unify quote usages

--- mpv-splice.lua
+++ mpv-splice.lua
@@ -120,25 +120,25 @@ local msg = require 'mp.msg'
 --------------------------------------------------------------------------------
 -- Default variables
 
-local default_tmp_location = "/tmp"
-local default_output_location = mp.get_property("working-directory")
+local default_tmp_location = '/tmp'
+local default_output_location = mp.get_property('working-directory')
 
 --------------------------------------------------------------------------------
 
-local concat_name = "concat.txt"
+local concat_name = 'concat.txt'
 
-local ffmpeg = "ffmpeg -hide_banner -loglevel warning"
+local ffmpeg = 'ffmpeg -hide_banner -loglevel warning'
 
-local tmp_location = os.getenv("MPV_SPLICE_TEMP")
-	and os.getenv("MPV_SPLICE_TEMP")
+local tmp_location = os.getenv('MPV_SPLICE_TEMP')
+	and os.getenv('MPV_SPLICE_TEMP')
 	or default_tmp_location
-local output_location = os.getenv("MPV_SPLICE_OUTPUT")
-	and os.getenv("MPV_SPLICE_OUTPUT")
+local output_location = os.getenv('MPV_SPLICE_OUTPUT')
+	and os.getenv('MPV_SPLICE_OUTPUT')
 	or default_output_location
 
 local times = {}
 local start_time = nil
-local remove_val = ""
+local remove_val = ''
 
 local exit_time = 0
 
@@ -146,14 +146,14 @@ local exit_time = 0
 
 function notify(duration, ...)
 	local args = {...}
-	local text = ""
+	local text = ''
 
 	for i, v in ipairs(args) do
 		text = text .. tostring(v)
 	end
 
 	msg.info(text)
-	mp.command(string.format("show-text \"%s\" %d 1",
+	mp.command(string.format('show-text "%s" %d 1',
 		text, duration))
 end
 
@@ -171,11 +171,11 @@ end
 
 function put_time()
 	local time = get_time()
-	local message = ""
+	local message = ''
 
 	if not start_time then
 		start_time = time
-		message = "[START TIMESTAMP]"
+		message = '[START TIMESTAMP]'
 	else
 		--times[#times+1] = {
 		table.insert(times, {
@@ -184,20 +184,20 @@ function put_time()
 		})
 		start_time = nil
 
-		message = "[END TIMESTAMP]"
+		message = '[END TIMESTAMP]'
 	end
 
-	notify(2000, message, ": ", time)
+	notify(2000, message, ': ', time)
 end
 
 function show_times()
-	notify(2000, "Total cuts: ", #times)
+	notify(2000, 'Total cuts: ', #times)
 
 	for i, obj in ipairs(times) do
-		msg.info("Slice", i, ": ", obj.t_start, " -> ", obj.t_end)
+		msg.info('Slice', i, ': ', obj.t_start, ' -> ', obj.t_end)
 	end
 	if start_time then
-		notify(2000, "Slice ", #times+1, " in progress.")
+		notify(2000, 'Slice ', #times+1, ' in progress.')
 	end
 end
 
@@ -210,15 +210,15 @@ function reset_current_slice()
 end
 
 function delete_slice()
-	if remove_val == "" then
-		notify(2000, "Entered slice deletion mode.")
+	if remove_val == '' then
+		notify(2000, 'Entered slice deletion mode.')
 
 		-- Add shortcut keys to the interval {0..9}.
 		for i=0,9,1 do
-			mp.add_key_binding("Alt+" .. i, "num_key_" .. i,
+			mp.add_key_binding('Alt+' .. i, 'num_key_' .. i,
 				function()
 					remove_val = remove_val .. i
-					notify(1000, "Slice to remove: "
+					notify(1000, 'Slice to remove: '
 						.. remove_val)
 				end
 			)
@@ -226,18 +226,18 @@ function delete_slice()
 	else
 		-- Remove previously added shortcut keys.
 		for i=0,9,1 do
-			mp.remove_key_binding("num_key_" .. i)
+			mp.remove_key_binding('num_key_' .. i)
 		end
 
 		remove_num = tonumber(remove_val)
 		if #times >= remove_num and remove_num > 0 then
 			table.remove(times, remove_num)
-			notify(2000, "Removed slice ", remove_num)
+			notify(2000, 'Removed slice ', remove_num)
 		end
 
-		remove_val = ""
+		remove_val = ''
 
-		msg.info("Exited slice deletion mode.")
+		msg.info('Exited slice deletion mode.')
 	end
 end
 
@@ -248,14 +248,14 @@ function prevent_quit(name)
 		else
 			exit_time = os.time()
 		end
-		notify(3000, "Slice has been marked. Press again to quit")
+		notify(3000, 'Slice has been marked. Press again to quit')
 	else
 		mp.command(name)
 	end
 end
 
 function process_video()
-	local alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	local alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 	local rnd_size = 10
 
 	local pieces = {}
@@ -265,64 +265,64 @@ function process_video()
 	math.random(); math.random(); math.random()
 
 	if times[#times] then
-		local tmp_dir = io.popen(string.format("mktemp -d -p %s",
-			tmp_location)):read("*l")
-		local input_file = mp.get_property("path")
-		local ext = string.gmatch(input_file, ".*%.(.*)$")()
+		local tmp_dir = io.popen(string.format('mktemp -d -p %s',
+			tmp_location)):read('*l')
+		local input_file = mp.get_property('path')
+		local ext = string.gmatch(input_file, '.*%.(.*)$')()
 
-		local rnd_str = ""
+		local rnd_str = ''
 		for i=1,rnd_size,1 do
 			local rnd_index = math.floor(math.random() * #alphabet + 0.5)
 			rnd_str = rnd_str .. alphabet:sub(rnd_index, rnd_index)
 		end
 
-		local output_file = string.format("%s/%s_%s_cut.%s",
+		local output_file = string.format('%s/%s_%s_cut.%s',
 			output_location,
-			mp.get_property("filename/no-ext"),
+			mp.get_property('filename/no-ext'),
 			rnd_str, ext)
 
-		local cat_file_name = string.format("%s/%s", tmp_dir, "concat.txt")
-		local cat_file_ptr = io.open(cat_file_name, "w")
+		local cat_file_name = string.format('%s/%s', tmp_dir, 'concat.txt')
+		local cat_file_ptr = io.open(cat_file_name, 'w')
 
-		notify(2000, "Process started!")
+		notify(2000, 'Process started!')
 
 		for i, obj in ipairs(times) do
-			local path = string.format("%s/%s_%d.%s",
+			local path = string.format('%s/%s_%d.%s',
 				tmp_dir, rnd_str, i, ext)
 			cat_file_ptr:write(string.format("file '%s'\n", path))
-			os.execute(string.format("%s -ss %s -i \"%s\" -to %s " ..
-				"-c copy -copyts -avoid_negative_ts make_zero \"%s\"",
+			os.execute(string.format('%s -ss %s -i "%s" -to %s ' ..
+				'-c copy -copyts -avoid_negative_ts make_zero "%s"',
 				ffmpeg, obj.t_start, input_file, obj.t_end,
 				path))
 		end
 
 		cat_file_ptr:close()
 
-		cmd = string.format("%s -f concat -safe 0 -i \"%s\" " ..
-			"-c copy \"%s\"",
+		cmd = string.format('%s -f concat -safe 0 -i "%s" ' ..
+			'-c copy "%s"',
 			ffmpeg, cat_file_name, output_file)
 		os.execute(cmd)
 
-		notify(10000, "File saved as: ", output_file)
-		msg.info("Process ended!")
+		notify(10000, 'File saved as: ', output_file)
+		msg.info('Process ended!')
 
-		os.execute(string.format("rm -rf %s", tmp_dir))
-		msg.info("Temporary directory removed!")
+		os.execute(string.format('rm -rf %s', tmp_dir))
+		msg.info('Temporary directory removed!')
 	end
 end
 
-mp.set_property("keep-open", "yes") -- Prevent mpv from exiting when the video ends
-mp.set_property("quiet", "yes") -- Silence terminal.
+mp.set_property('keep-open', 'yes') -- Prevent mpv from exiting when the video ends
+mp.set_property('quiet', 'yes') -- Silence terminal.
 
-mp.add_key_binding('q', "quit", function()
-	prevent_quit("quit")
+mp.add_key_binding('q', 'quit', function()
+	prevent_quit('quit')
 end)
-mp.add_key_binding('Shift+q', "quit-watch-later", function()
-	prevent_quit("quit-watch-later")
+mp.add_key_binding('Shift+q', 'quit-watch-later', function()
+	prevent_quit('quit-watch-later')
 end)
 
-mp.add_key_binding('Alt+t', "put_time", put_time)
-mp.add_key_binding('Alt+p', "show_times", show_times)
-mp.add_key_binding('Alt+c', "process_video", process_video)
-mp.add_key_binding('Alt+r', "reset_current_slice", reset_current_slice)
-mp.add_key_binding('Alt+d', "delete_slice", delete_slice)
+mp.add_key_binding('Alt+t', 'put_time', put_time)
+mp.add_key_binding('Alt+p', 'show_times', show_times)
+mp.add_key_binding('Alt+c', 'process_video', process_video)
+mp.add_key_binding('Alt+r', 'reset_current_slice', reset_current_slice)
+mp.add_key_binding('Alt+d', 'delete_slice', delete_slice)
